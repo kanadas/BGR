f698e67b6ae546321ff347385172f3927e6e6b88
Tomasz Kanas <tk385674@students.mimuw.edu.pl>
2018-01-09 21:57:51 +0100

    tagtype.sql

--- schema/tagtypes.sql
+++ schema/tagtypes.sql
@@ -1,5 +1,10 @@
-INSERT INTO TagType (name, weight) VALUES
-	("Type", "0,25"),
-	("Category", "0,25"),
-	("Mechanisms", "0,25"),
-	("Family", "0,25");
+CREATE PROCEDURE INS(p_name TagType.name%TYPE, p_weight TagType.weight%TYPE) IS 
+BEGIN
+	INSERT INTO TagType (name, weight) VALUES (p_name, p_weight);
+END INS;
+/ 
+EXECUTE INS('Type', '0.25');
+EXECUTE INS('Category', '0.25');
+EXECUTE INS('Mechanisms', '0.25');
+EXECUTE INS('Family', '0.25');
+DROP PROCEDURE INS;
